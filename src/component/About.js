6df970c1fc8b3c1185fe58f8cf19a1b486db850c
michaelseptiadi <74148613+michaelseptiadi@users.noreply.github.com>
2021-08-05 17:06:04 +0700

    Updated

--- src/component/About.js
+++ src/component/About.js
@@ -8,17 +8,12 @@ export default function About() {
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                         Hi, Im Michael.
                         <br />
-                        Its my Portofolio Website.
                     </h1>
                     <p className="mb-8 leading-relaxed text-white text-justify">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the
-                        1500s, when an unknown printer took a galley of type and scrambled it to
-                        make a type specimen book. It has survived not only five centuries, but also
-                        the leap into electronic typesetting, remaining essentially unchanged. It
-                        was popularised in the 1960s with the release of Letraset sheets containing
-                        Lorem Ipsum passages, and more recently with desktop publishing software
-                        like Aldus PageMaker including versions of Lorem Ipsum.
+                        <b>Its my Portofolio Website using ReactJS and Tailwind.</b>
+                        <br />I love building websites, I love design, I love everything about
+                        websites. Building a website for me is like being in a game that makes us
+                        think and be more creative.
                     </p>
                     <div className="flex justify-content">
                         <a
